2fc43b5764040ce22eda962e2834a04f6874c6f7
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2019-10-12 17:39:21 +0200

    open image, rotate to 180°, return base 64 data

---
+++ back/main.cpp
@@ -0,0 +1,65 @@
+#include <fstream>
+
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
+
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+
+  if (i)
+  {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
+
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    char_array_4[3] = char_array_3[2] & 0x3f;
+
+    for (j = 0; (j < i + 1); j++)
+      ret += base64_chars[char_array_4[j]];
+
+    while((i++ < 3))
+      ret += '=';
+
+  }
+
+  return ret;
+
+}
+
+
+std::string rotateImage(std::string filename)
+{
+	std::ifstream file(filename, "r");
+
+	file.seekg(file.end());
+	int len = file.tellg();
+	char* data = new char[len];
+	file.read(data, len);
+	file.close();
+	
+	std::string binary;
+	
+	for(int i = len - 1; i >= 0; i--)
+		binary.push_back(data[i]);
+	
+	delete data;
+	
+	return base64_encode(binary.c_str(), len);
+}
